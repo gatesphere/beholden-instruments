d525149c7672d7a86e93b99067e01cdbceda63aa
Jacob Peck <gatesphere@gmail.com>
2017-08-11 11:35:25 -0400

    lamewing-modular now object-oriented, kinda...

--- lamewing-modular/lamewing-modular.ino
+++ lamewing-modular/lamewing-modular.ino
@@ -3,6 +3,30 @@
 // see original at: 
 // http://kinetiklaboratories.blogspot.com/p/harsh-noise-generator.html
 
+class Oscillator {
+  int value;  // current output of the oscillator -- 0/1 for digital oscs, 0-255 for analog oscs
+  unsigned long before; // micros() time of previous tick
+  
+  public:
+  Oscillator(int a_value = 0) {
+    value = a_value;
+    before = 0;
+  }
+  
+  void update(int new_value, long new_before) {
+    value = new_value;
+    before = new_before;
+  }
+  
+  void flip(long new_before) {
+    update(!value, new_before);
+  }
+}
+
+
+  
+    
+
 // MODE LAYOUT:
 // 0 - dual osc mode
 // 1 - square + noise osc mode
@@ -13,8 +37,6 @@
 // pins
 #define INTENSITY A0
 #define DENSITY A1
-#define INTENSITY_CV A2  //pulled down to ground
-#define DENSITY_CV A3    //pulled down to ground
 #define OUT_DUAL 7 // dual osc out (mode 0)
 #define OUT_MERZ 8 // square + noise out (mode 1)
 #define OUT_WN 9 // white noise out (PWM pin, mode 2)
@@ -23,46 +45,32 @@
 
 
 // Variables
-int osc0_dual = 0;                      // First oscillator
-int osc0_merz = 0;
-int osc0_wn = 0;
-int osc0_drone = 0;
-int osc0_noise = 0;
-int osc1_dual = 0;                      // Second oscillator
-int osc1_merz = 0;
-int osc1_drone = 0;
+Oscillator osc0_dual = Oscillator();
+Oscillator osc1_dual = Oscillator();
+Oscillator osc0_merz = Oscillator();
+Oscillator osc1_merz = Oscillator();
+Oscillator osc0_wn = Oscillator();
+Oscillator osc0_drone = Oscillator();
+Oscillator osc1_drone = Oscillator();
+Oscillator osc0_noise = Oscillator();
+
 int oscOut = 0;                         // Output
 int amplitude = 0;
 int frequency = 0;
 int out_intensity = 0;
 int out_density = 0;
 long delta = 0;
-float intensity_pot = 0;
-float intensity_cv = 0;
-float density_pot = 0;
-float density_cv = 0;
 long intensity = 0;
 long density = 0;
 long now = 0;
-long before0_dual = 0;
-long before0_merz = 0;
-long before0_wn = 0;
-long before0_drone = 0;
-long before0_noise = 0;
-long before1_dual = 0;
-long before1_merz = 0;
-long before1_drone = 0;
 long timeOscillator0 = 0;
 long timeOscillator0_noise = 0;
 long timeOscillator1 = 0;
 int mode_noise = true; // mode 4 noise -- true = making noise, false = silent
 
-
 void setup() {
   pinMode(INTENSITY, INPUT);
-  pinMode(INTENSITY_CV, INPUT);
   pinMode(DENSITY, INPUT);
-  pinMode(DENSITY_CV, INPUT);
   pinMode(OUT_DUAL, OUTPUT);
   pinMode(OUT_MERZ, OUTPUT);
   pinMode(OUT_WN, OUTPUT);
@@ -76,94 +84,66 @@ void loop()
 {
   // basic setup for the rest
   now = micros();
-  
-  intensity_pot = analogRead(INTENSITY);
-  intensity_cv = analogRead(INTENSITY_CV);
-  intensity_cv > 0 ? intensity = intensity_cv * intensity_pot / 1023 : intensity = intensity_pot;
-  //intensity = analogRead(INTENSITY);
-  //Serial.println("intensity");
-  //Serial.println(intensity_pot);
-  //Serial.println(intensity_cv);
-  //Serial.println(intensity);
 
-  density_pot = analogRead(DENSITY);
-  density_cv = analogRead(DENSITY_CV);
-  density_cv > 0 ? density = density_cv * density_pot / 1023 : density = density_pot;
-  //density = analogRead(DENSITY);
-  //Serial.println("density");
-  //Serial.println(density_pot);
-  //Serial.println(density_cv);
-  //Serial.println(density);
-  //Serial.println("===");
-  //delay(1000);
+  intensity = analogRead(INTENSITY); // CV handled in hardware via op-amps
+  density = analogRead(DENSITY); // CV handled in hardware via op-amps
   
   // mode 0 - dual oscillator mode
   timeOscillator0 = map(intensity, 0, 1023, 50000, 600) + random(1000);
   timeOscillator1 = map(density, 0, 1023, 50000, 600) + random(1000);
-  if (now - before0_dual > timeOscillator0) {
-    before0_dual = now;
-    osc0_dual = !osc0_dual;
+  if (now - osc0_dual.before > timeOscillator0) {
+    osc0_dual.flip(now);
   }
-  if (now - before1_dual > timeOscillator1) {
-    before1_dual = now;
-    osc1_dual = !osc1_dual;
+  if (now - osc1_dual.before > timeOscillator1) {
+    osc1_dual.flip(now);
   }
-  oscOut = osc0_dual ^ osc1_dual;
+  oscOut = osc0_dual.value ^ osc1_dual.value;
   digitalWrite(OUT_DUAL, oscOut);
 
-  /*
   // mode 1 - square and noise oscs
   timeOscillator0 = map(intensity, 0, 1023, 10000, 100);
   timeOscillator1 = map(density, 0, 1023, 10000, 0);
-  if (now - before0_merz > timeOscillator0) {
-    before0_merz = now;
-    osc0_merz = !osc0_merz;
+  if (now - osc0_merz.before > timeOscillator0) {
+    osc0_merz.flip(now);
   }
-  if (now - before1_merz > timeOscillator1) {
-    before1_merz = now;
-    osc1_merz = random(0,2); // 0 or 1
+  if (now - osc1_merz.before > timeOscillator1) {
+    osc1_merz.update(random(0,2), now);
   }
-  oscOut = osc0_merz ^ osc1_merz;
+  oscOut = osc0_merz.value ^ osc1_merz.value;
   digitalWrite(OUT_MERZ, oscOut);
 
   // mode 2 - 'white noise'
   amplitude = map(intensity, 0, 1023, 0, 255);
   frequency = map(density, 0, 1023, 10000, 0);
-  if (now - before0_wn > frequency) {
-    before0_wn = now;
-    osc0_wn = random(0,2);
+  if (now - osc0_wn.before > frequency) {
+    osc0_wn.update(random(0,2), now);
   }
-  oscOut = osc0_wn * amplitude;
+  oscOut = osc0_wn.value * amplitude;
   analogWrite(OUT_WN, oscOut);
   
   // mode 3 - drone mode
   timeOscillator0 = map(intensity, 0, 1023, 50000, 400);
   timeOscillator1 = map(density, 0, 1023, 50000, 0);
-  if (now - before0_drone > timeOscillator0) {
-    before0_drone = now;
-    osc0_drone = !osc0_drone;
+  if (now - osc0_drone.before > timeOscillator0) {
+    osc0_drone.flip(now);
   }
-  if (now - before1_drone > timeOscillator1) {
-    before1_drone = now;
-    osc1_drone = !osc1_drone;
+  if (now - osc1_drone.before > timeOscillator1) {
+    osc1_drone.flip(now);
   }
-  oscOut = osc0_drone ^ osc1_drone;
+  oscOut = osc0_drone.value ^ osc1_drone.value;
   digitalWrite(OUT_DRONE, oscOut);
   
   // mode 4 - noise mode
-  if (now - before0_noise > timeOscillator0_noise) {
-    before0_noise = now;
+  if (now - osc0_noise.before > timeOscillator0_noise) {
+    osc0_noise.flip(now);
     out_density = map(density, 0, 1024, 1014, 10);
-    delta = random(out_density) * 500000000; // this 5000 will need to be tweaked
-    mode_noise = !mode_noise;
-    mode_noise ? timeOscillator0_noise = delta : timeOscillator0_noise = delta * 3;
+    timeOscillator0_noise = random(out_density) * 1000; // this value will need to be tweaked
+    if (!osc0_noise.value) {
+      timeOscillator0_noise *= 3;
+    }
   }
-  if (mode_noise) { // making noise
-    out_intensity = map(intensity, 0, 1023, 1, 255);
-    analogWrite(OUT_NOISE, random(out_intensity));
-  } else { // silent
-    analogWrite(OUT_NOISE, 0);
-  }
+  out_intensity = map(intensity, 0, 1023, 1, 255);
+  analogWrite(OUT_NOISE, osc0_noise.value * random(out_intensity));
     
   /*
   // original code here -- the above was just a guess at being functionally equivalent
@@ -176,7 +156,7 @@ void loop()
       analogWrite(OUT, x);
     }
     analogWrite(OUT, 0);
-    delay(random(density) * 3);
+    delay(random(density) * 3); // between 3042 and 30 milliseconds
   }
   */
 }
